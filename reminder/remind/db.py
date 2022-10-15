9649d9ae730490243a8ba85b9759ebea4ac3e32e
kunansy <kolobov.kirill@list.ru>
2022-10-15 08:25:57 +0300

    db skeleton added

---
+++ reminder/remind/db.py
@@ -0,0 +1,25 @@
+from uuid import UUID
+
+from reminder.remind import schemas
+
+
+async def _get_notes_count() -> int:
+    pass
+
+
+async def _get_random_note_offset() -> int:
+    pass
+
+
+async def get_random_note() -> schemas.Note:
+    pass
+
+
+async def _get_last_material_remind(material_id: UUID) -> LastMaterialRemind:
+    pass
+
+
+async def insert_notes_history(*,
+                               note_id: UUID,
+                               user_id: int) -> None:
+    pass
