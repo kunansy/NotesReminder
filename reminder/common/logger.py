7010b95bdbff285b382750e2f43e0e38adde9200
kunansy <kolobov.kirill@list.ru>
2022-10-15 07:48:24 +0300

    logger added

---
+++ reminder/common/logger.py
@@ -0,0 +1,22 @@
+import logging
+
+from reminder.common import settings
+
+
+MSG_FMT = "{levelname:<8} [{asctime},{msecs:3.0f}] [PID:{process}] " \
+          "[{filename}:{funcName}():{lineno}] {message}"
+DATE_FMT = "%d.%m.%Y %H:%M:%S"
+
+formatter = logging.Formatter(
+    fmt=MSG_FMT, datefmt=DATE_FMT, style='{'
+)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter)
+
+logger = logging.getLogger(settings.LOGGER_NAME)
+logger.setLevel(settings.LOGGER_LEVEL)
+
+logger.addHandler(stream_handler)
+logger.info("Logger configured with level=%s", logger.level)
