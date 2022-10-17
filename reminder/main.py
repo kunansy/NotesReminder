d1768bbbc00f3871b734b6ed5e996d5f8e8bbea0
kunansy <kolobov.kirill@list.ru>
2022-10-17 11:37:03 +0300

    CLI args added

--- reminder/main.py
+++ reminder/main.py
@@ -1,8 +1,13 @@
+import argparse
+import asyncio
+
 from aiogram import types
 from aiogram.utils import executor
 
+from reminder.common import settings
 from reminder.common.bot_api import restrict_access, bot, dp, send_msg
 from reminder.common.logger import logger
+from reminder.remind import routes as remind_routes
 
 
 async def set_default_commands() -> None:
@@ -31,5 +36,35 @@ async def start_bot(msg: types.Message) -> None:
     await send_msg(funcs_description)
 
 
+def args_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Start Telegram bot or send a message with it"
+    )
+    parser.add_argument(
+        '--start',
+        action="store_true",
+        default=False,
+        dest='start',
+    )
+    parser.add_argument(
+        '--remind',
+        action="store_true",
+        default=False,
+        dest='start',
+        help="Send a new note to remind"
+    )
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = args_parser()
+
+    if args.start:
+        executor.start_polling(dp)
+    if args.remind:
+        asyncio.run(remind_routes.remind(user_id=settings.TG_BOT_USER_ID))
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    main()
