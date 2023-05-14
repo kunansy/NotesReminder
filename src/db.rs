eb0b08e13a84ca50731b21cafc6b950454f14ccc
kunansy <kolobov.kirill@list.ru>
2023-05-14 14:03:33 +0300

    get_remind_statistics() implemented

--- src/db.rs
+++ src/db.rs
@@ -31,8 +31,29 @@ pub mod db {
         RemindInfo(1, chrono::NaiveDateTime)
     }
 
-    async fn get_remind_statistics(pool: &PgPool) -> HashMap<String, i32> {
-        HashMap::new()
+    async fn get_remind_statistics(pool: &PgPool) -> Result<HashMap<String, i64>, sqlx::Error> {
+        let stat = sqlx::query!(
+            "
+            WITH stats AS (
+                SELECT note_id, count(1)
+                FROM note_repeats_history
+                GROUP BY note_id
+            )
+            SELECT
+                n.note_id AS note_id,
+                COALESCE(s.count, 0) AS count
+            FROM notes n
+            LEFT JOIN stats s USING(note_id)
+            WHERE NOT n.is_deleted;
+            "
+        )
+            .fetch_all(pool)
+            .await?
+            .iter()
+            .map(|row| {(row.note_id.to_string(), row.count.unwrap())})
+            .collect::<HashMap<String, i64>>();
+
+        Ok(stat)
     }
 
     fn get_remind_note_id(stats: HashMap<String, i32>) -> String {
