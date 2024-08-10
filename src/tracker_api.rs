4dbac6678145c4578189596eef69c1c7046e8008
kunansy <kolobov.kirill@list.ru>
2024-08-10 10:29:45 +0300

    improve logs

--- src/tracker_api.rs
+++ src/tracker_api.rs
@@ -19,19 +19,19 @@ pub mod tracker_api {
     }
 
     pub async fn get_repeat_queue(tracker_url: &str) -> Result<Vec<RepeatItem>, String> {
-        log::debug!("Getting repeat queue");
-        let client = Client::new();
-
         let url = format!("{}/materials/repeat-queue", tracker_url).parse()
             .map_err(|e: uri::InvalidUri| e.to_string())?;
 
+        log::debug!("Getting repeat queue from {}", url);
+        let client = Client::new();
+
         let resp = client.get(url)
-            .await.map_err(|e| e.to_string())?;
+            .await.map_err(|e| format!("GET fails: {}", e.to_string()))?;
         let body = hyper::body::aggregate(resp)
             .await.map_err(|e| e.to_string())?;
 
         let json: Vec<RepeatItem> = serde_json::from_reader(body.reader())
-            .map_err(|e| e.to_string())?;
+            .map_err(|e| format!("Could not parse json: {}", e.to_string()))?;
 
         log::debug!("{} queue items found", &json.len());
         Ok(json)
